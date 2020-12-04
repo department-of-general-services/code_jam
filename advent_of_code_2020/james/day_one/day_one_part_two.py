88898966f44bdb5d48d2536d888a96e95a93bb4a
James Trimarco <james.trimarco@gmail.com>
2020-12-04 11:27:27 -0500

    james' solutions for days 1, 2, 3 (part 1)

---
+++ advent_of_code_2020/james/day_one/day_one_part_two.py
@@ -0,0 +1,55 @@
+import functools
+import time
+from pathlib import Path
+from itertools import combinations
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        times = 10
+        tic = time.perf_counter()
+        for iteration in range(times):
+            value = func(*args, **kwargs)
+        toc = time.perf_counter()
+        elapsed_time = (toc - tic) / times
+        print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+        return value
+
+    return wrapper_timer
+
+
+@timer
+def adds_to_2020_brute_force(input_path):
+    entries = open(input_path).read().splitlines()
+    entries = list(map(int, entries))
+
+    for entry in entries:
+        rest_of_entries = [item for item in entries if item != entry]
+        for second_entry in rest_of_entries:
+            rest_of_entries = [item for item in entries if item != second_entry]
+            for third_entry in rest_of_entries:
+                if entry + second_entry + third_entry == 2020:
+                    # print(f"Found it! {entry}, {second_entry}, {third_entry}")
+                    return entry * second_entry * third_entry
+
+
+@timer
+def adds_to_2020_combinations(input_path):
+    entries = open(input_path).read().splitlines()
+    entries = list(map(int, entries))
+
+    for trio in combinations(entries, r=3):
+        if sum(trio) == 2020:
+            # print(f"Found it! {trio[0]}, {trio[1]}, {trio[2]}")
+            return trio[0] * trio[1] * trio[2]
+
+
+if __name__ == "__main__":
+    input_path = Path.cwd() / "day_one" / "input_day_1.txt"
+    print("Running brute force method.")
+    answer = adds_to_2020_brute_force(input_path)
+    print(f"The answer is: {answer}")
+    print("Running itertools method.")
+    answer = adds_to_2020_combinations(input_path)
+    print(f"The answer is: {answer}")
---
+++ advent_of_code_2020/james/day_two/day_two.py
@@ -0,0 +1,55 @@
+import functools
+import time
+from pathlib import Path
+from itertools import combinations
+import pandas as pd
+import re
+
+
+def split_input(row):
+    regex = r"^(\d+)-(\d+)\s(\w):\s(\w+)"
+    match = re.match(pattern=regex, string=row["raw_text"])
+    row["min_reps"] = int(match.groups()[0])
+    row["max_reps"] = int(match.groups()[1])
+    row["target_char"] = match.groups()[2]
+    row["password"] = match.groups()[3]
+    return row
+
+
+def is_password_legit(row):
+    # count the number of times the char in question appears
+    row["count"] = row["password"].count(row["target_char"])
+    # check that the count is not less than the min or more than the max
+    row["is_valid"] = row["min_reps"] <= row["count"] <= row["max_reps"]
+    return row
+
+
+def is_password_legit_part_II(row):
+    # rename the changed cols to avoid confusion
+    row = row.rename({"min_reps": "pos_1", "max_reps": "pos_2"})
+    # switch to 0-based indexing
+    row["pos_1"] = row["pos_1"] - 1
+    row["pos_2"] = row["pos_2"] - 1
+    # creating boolean columns for positions 1 & 2
+    row["is_in_pos_1"] = row["password"][row["pos_1"]] == row["target_char"]
+    row["is_in_pos_2"] = row["password"][row["pos_2"]] == row["target_char"]
+    # the two boolean cols need to sum to 1 (exclusive or)
+    row["is_valid"] = sum(row[["is_in_pos_1", "is_in_pos_2"]]) == 1
+    return row
+
+
+if __name__ == "__main__":
+    input_path = Path.cwd() / "day_two" / "input_day_2.txt"
+    passwords = open(input_path).read().splitlines()
+    df = pd.DataFrame(passwords, columns=["raw_text"])
+    df_split = df.apply(split_input, axis=1)
+
+    ### Part I
+    res_df = df_split.apply(is_password_legit, axis=1)
+    print("Part I")
+    print(f"Of {len(res_df)} passwords, {res_df['is_valid'].sum()} are legit.")
+
+    ### Part II
+    res_df_2 = df_split.apply(is_password_legit_part_II, axis=1)
+    print("Part II")
+    print(f"Of {len(res_df_2)} passwords, {res_df_2['is_valid'].sum()} are legit.")
